9d8f9e32747f4bae3c87029815ae30714ae4c86d
Avesta03 <avestaafsharimehr@gmail.com>
2024-11-05 17:18:07 +0000

    Debugged, Utilised FOR, Implemented Symbolic Const

---
+++ FahrenheitCelsiusTable3.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+
+
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
+/* Utilising the 'For' Statement */
+
+int main()
+{
+    int fahr;
+    
+    for (fahr = 0; fahr <= 300; fahr = fahr + 20)
+        printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
+    
+    return 0; /* This is optional from C99, but indicates successful completion of the 'main' function */
+}
+
